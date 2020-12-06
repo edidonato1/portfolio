70209de82dc4dc9334769d33927ba1c8e102bbc8
Eddie <edward.f.didonato@gmail.com>
2020-12-06 15:32:24 -0500

    add animation to name home

--- portfolio/src/screens/Home/Home.jsx
+++ portfolio/src/screens/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import { Link } from 'react-router-dom';
+import Layout from '../../components/shared/Layout/Layout';
 import HomeDiv from './StyledHome';
 
 export default function Home() {
@@ -12,7 +12,18 @@ export default function Home() {
           <div className="left-div">
             <div className="left-div-child">
               <div className="title">
-                <h1>hello, <br></br> i'm eddie.</h1>
+                <h1>hello, <br></br><span className="introduction">
+                  i'm 
+                    <Link className="name" to="/about">
+                    <div id="e1"> e</div>
+                    <div id="d1">d</div>
+                    <div id="d2">d</div>
+                    <div id="i">i</div>
+                    <div id="e2">e</div>
+                    <div id="dot">.</div>
+                    </Link>
+                </span>
+                </h1>
                 <h2>web developer</h2>
                 <small>front end  |  full stack</small>
               </div>
--- portfolio/src/screens/Home/StyledHome.js
+++ portfolio/src/screens/Home/StyledHome.js
@@ -9,6 +9,57 @@ display: flex;
   h2 {
     margin-top: 0px;
   }
+
+  @keyframes bounce {
+    0% {transform: translateY(0%)}
+    5% {transform: translateY(-10%)}
+    10% {transform: translateY(0%)}
+   100% {transform: translateY(0%)}
+  }
+
+  .introduction {
+    display: flex;
+    align-items: flex-end;
+  }
+
+
+
+  .name {
+    color: inherit;
+    margin-left: 7px;
+    display: flex;
+    font-size: 40px;
+    -webkit-text-stroke: .8px #f0ebeb;
+    text-shadow: 2px 2px #35a3a3;
+    /* letter-spacing: 1px; */
+  }
+
+  #e1 {
+    animation: 8.5s ease-in-out 1s infinite bounce;
+
+  }
+
+  #d1 {
+    animation: 8.5s ease-in-out 1.2s infinite bounce;
+
+  }
+
+  #d2 {
+    animation: 8.5s ease-in-out 1.4s infinite bounce;
+  }
+
+  #i {
+    animation: 8.5s ease-in-out 1.6s infinite bounce;
+  }
+
+  #e2 {
+    animation: 8.5s ease-in-out 1.8s infinite bounce;
+  }
+
+  #dot {
+    animation: 8.5s ease-in-out 2s infinite bounce;
+  }
+
   .title {
   }
 
